9b1dc8f96486e789371c0bd07520a14c05312bda
Daitarn <roberto.rutigliano@gmail.com>
2018-05-08 08:33:30 +0200

    Test MaterialDesign

---
+++ sito3.3/Controllers/TestMaterialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApplication.Controllers
+{
+    public class TestMaterialController : Controller
+    {
+        // GET: TestMaterial
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
